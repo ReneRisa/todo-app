98a1314b1e079ec30fdd08073c074421b9ebf70d
ReneRisa <risael31@gmail.com>
2023-11-05 13:11:34 -0500

    created a button to add task to the list

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from'react';
 import TodoList from './TodoList';
+import AddTask from './AddTask';
 
 function App() {
 
-  const[todos, setTodos] = useState([]) //state of todos in an array
-  todos.push('Go Shopping')
-  todos.push('Go for a run')
+  const [todos, setTodos] = useState([]) //state of todos in an array
+  const [completed, setCompleted] = useState(false) //state of completed todos
+
+  const toggleCompleted = (todo) => {
+    setCompleted(true)
+  }
+
+  const removeTodo = (index) => {
+    setTodos(todos.filter((todo, i) => i!== index))
+  }
+
+  const addTodo = (todo) => {
+    setTodos([...todos, todo])
+  }
   return (
     <div className="App">
+      <AddTask addTodo={addTodo} completed={completed}/>
       <TodoList todos={todos} />
     </div>
   );
